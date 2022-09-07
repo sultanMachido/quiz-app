69848ca99b648503bd394a9a2091d3953d589201
Ope <opehardesina@gmail.com>
2022-09-07 21:41:09 +0100

    commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from './components/Home';
+
+import QuizInstructions from './components/quiz/QuizInstructions';
+import Play from './components/quiz/Play';
+import QuizSummary from './components/quiz/QuizSummary';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/" exact component={Home}/>
+        <Route path="/play/instructions" exact component={QuizInstructions} />
+        <Route path="/play/quiz" exact component={Play}/>
+        <Route path="/play/quizSummary" exact component={QuizSummary}/>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
